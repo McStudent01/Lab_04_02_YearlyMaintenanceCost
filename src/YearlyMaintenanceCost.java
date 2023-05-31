e6e009ef81a6cea3ffcc6d646fdc4839df6a8d6e
McStudent01 <mcmillcy@mail.uc.edu>
2023-05-31 10:12:40 -0500

    Initial commit

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,14 @@
+public class YearlyMaintenanceCost
+{
+    public static void main(String[] args)
+    {
+
+        double springCost = 250.00;
+        double summerCost = 1000.50;
+        double fallCost = 75.00;
+        double winterCost = 425.00;
+        double totalCost = springCost + fallCost + winterCost + summerCost;
+
+        System.out.println("The total yearly cost is " +totalCost + " and this was found by adding the cost of Spring maintenance of " +springCost + " , the summer maintenance of " +summerCost + " , the fall maintenance of " +fallCost + " , and the winter maintencance of " +winterCost + " together.");
+    }
+}
